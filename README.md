3bab65c3d229f4d8a5da88a84cb75db7010347cc
jaakko <jaakko.aakula@gmail.com>
2022-04-22 18:11:29 +0300

    uusi funktio array()

--- README.md
+++ README.md
@@ -1,11 +1,50 @@
 # haversineformula
-a npm package to calculate distance between coordinates
+a npm package to calculate distance between two and several coordinates
 
 npm install haversineformula
 
-the haversineformula function takes tree variables:
+you use the haversineformula with two different functions: 
+twoCoordinates and array
+
+the twoCoordinates function takes tree variables:
 the first coordinates, the second coordinates, and the type of measurement you want.
 
 coordinates are in this form: {longitude: 23.13123, latitude: 80,123}
 
 the types are these: 'mile', 'km', 'meter', 'foot', 'yard'
+
+the array function works the with the same principle, but there is only
+an array of coordinates and the type. 
+
+example of the functions: 
+
+const haversineformula = require('haversineformula')
+
+const array = [
+   {
+
+      latitude: 60.15476,
+      longitude: 24.9464283,
+
+    },
+    {
+
+      latitude: 60.1547396,
+      longitude: 24.9465404,
+
+    },
+     {
+
+      latitude: 60.1547365,
+      longitude: 24.9465579,
+
+    },
+   {
+      latitude: 59.8702617,
+      longitude: 30.3746517,
+    },
+  ]
+
+  console.log(haversineformula.array(array, 'meter'))
+
+  console.log(haversineformula.twoCoordinates(array[0], array[1], 'meter'))
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 
-function haversineformula(start, end, type) {
+function twoCoordinates(start, end, type) {
      
     const lat2 = end.latitude
     const lon2 = end.longitude
     const lat1 = start.latitude
     const lon1 = start.longitude
     
-    radius = type ? getRadius(type) : 6371
+    const radius = type ? getRadius(type) : 6371
 
     const startLatRad = radians(lat1)
     const endLatRad = radians(lat2)
 
-    const lonDiffRad = radians(lon1 - lon2)
+    const lonDiffRad = radians(lon2 - lon1)
 
     const d = Math.acos((Math.sin(startLatRad) * Math.sin(endLatRad)) + (Math.cos(startLatRad) * Math.cos(endLatRad) * Math.cos(lonDiffRad)))
 
@@ -36,5 +36,24 @@ function getRadius(type) {
     }
 }
 
+function array(array, type) {
+     
+    const radius = type ? getRadius(type) : 6371
+    let length = 0
 
-module.exports = haversineformula
+    for(let i = 1; i < array.length; i++ ) {
+        
+        const startLatRad = radians(array[i - 1].latitude)
+        const endLatRad = radians(array[i].latitude)
+    
+        const lonDiffRad = radians(array[i - 1].longitude - array[i].longitude)
+    
+        const d = Math.acos((Math.sin(startLatRad) * Math.sin(endLatRad)) + (Math.cos(startLatRad) * Math.cos(endLatRad) * Math.cos(lonDiffRad)))
+    
+        length = length + d * radius
+    }
+    
+    return length
+}
+
+module.exports = {twoCoordinates, array}
